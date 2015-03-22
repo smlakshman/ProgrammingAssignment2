585d2ddc1d0c15ebb0d23d57e45151e3d90e52b0
smlakshman <smlakshman@gmail.com>
2015-03-22 22:10:35 +0800

    Revert "Comments added"
    This reverts commit 2e702b3825fa4baa0eeada3b34aeebed393080a8.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-## This function creates a special "matrix" object that can cache its inverse.
-## The first time that cacheSolve is called on argument x , R computes the inverse matrix;
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
@@ -17,8 +17,8 @@ list(set=set, get=get,
    getmatrix=getmatrix)
 }
 
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
 m<-x$getmatrix()
